06496fab24155621ad4af4bf36b07f64ae837b9e
Greg Park <31253523+greg-park@users.noreply.github.com>
2022-01-31 13:01:29 -0600

    Main file used for testing

---
+++ gjp.py
@@ -0,0 +1,11 @@
+# Main Program for Python testing
+#
+# this program is used to call the functions used to setup
+# synergy frames and servers
+###
+
+# GJP updates.  Change to "main" that calls as a function
+import appliance_health_status as appliance_health
+
+# check the appliance health
+appliance_health()
