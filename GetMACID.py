65a70c00b7e3c427e8b32b53a127c2383aa8ca28
greg-park <gregory.park@hpe.com>
2022-07-25 09:30:26 -0500

    Update

--- GetMACID.py
+++ GetMACID.py
@@ -1,14 +1,19 @@
-# Simple set of loops to inventory a Synergy/OneView environment
+##############################################################################
+# GetMACID.py
+# - Script for listing the MAC IDs for servers with applied profiles.
 #
+#   VERSION 1.0
+#   Date:
+#   Input: config.json
+#   Output: Creates a file in a local subdirectory. jsonfiles, with
+#           list of Frame, Bay, MAC ID, Card
+############################################################################## 
 ###
 
-from fileinput import filename
 from pprint import pprint
+from fileinput import filename
 from ConfigLoader import try_load_from_file
 from hpeOneView.oneview_client import OneViewClient
-import json
-#import inspect
-#from itertools import ifilter
 
 config = {
     "ip": "<oneview_ip>",
@@ -18,26 +23,34 @@ config = {
     },
     "api_version": "<api_version>"
 }
-
-def save_file(data_file, outfile, m):
-     with open(file=outfile, mode=m) as output_file:
-         json.dump(data_file, output_file, indent=5)
  
-JSON_DIR = ".\jsonfiles\\"
 # Try load config from a file (if there is a config file)
 config = try_load_from_file(config)
 oneview_client = OneViewClient(config)
+JSON_DIR = ".\jsonfiles\\"
  
 print("Get list of all the server MAC IDs from appliance: ")
 servers = oneview_client.server_hardware.get_all()
+enclosure_resource = oneview_client.enclosures
+
 outfile=JSON_DIR+"serverMAC.json"
-open(outfile,"w")
-with open(file=outfile, mode="w") as output_file:
-    for svr in servers:
-        save_file(svr, outfile,"a+")
+of = open(outfile,"w")
+for svr in servers:
+#    if svr['state'] == "ProfileApplied":
         frame = svr['locationUri'].split("/")
+        enclosure = enclosure_resource.get_by_uri(svr['locationUri'])
         location = frame[len(frame)-1]
         bay = svr['position']
-        macID = location + ',' + str(bay) + ',' + svr['portMap']['deviceSlots'][0]['physicalPorts'][0]['wwn']
-        print(macID)
+        print ("Enclusore: {0:s} Bay: {1:s} ProfileSate: {2:s}".format(enclosure.data['name'],(str(bay)),svr['state']))
+        for slot in svr['portMap']['deviceSlots']:
+            macID = location + ',' + str(bay)
+            #of.write(macID)
+            if slot['deviceName'] != "":
+                print("\tDevice: {0:s}".format(slot['deviceName']))
+                portnum = 0
+                for port in slot['physicalPorts']:
+                    wwn = str(port['wwn'])
+                    print ("\t\tPort {0:d} {1:s}".format(portnum, wwn))
+                    portnum = portnum + 1
+of.close()
 
--- GetServersNoProfile.py
+++ GetServersNoProfile.py
@@ -1,8 +1,13 @@
-# Simple set of loops to inventory a Synergy/OneView environment
+##############################################################################
+# GetServersNoProfile.py
+# - Script for listing theservers with no profiles.
 #
-# Easily consumable by others
-# 
-###
+#   VERSION 1.0
+#   Date:
+#   Input: config.json
+#   Output: Creates a file in a local subdirectory. jsonfiles, with
+#           list of servers and suggested Profile Name
+############################################################################## 
 
 from fileinput import filename
 from pprint import pprint
@@ -19,61 +24,64 @@ config = {
         "password": "<password>",
     },
     "profile_name": "<profile_name>",
+    "profile_template": "<profile_template>",
+    "server_hardware_type": "<server_hardware_type>",
     "api_version": "<api_version>"
 }
 
-def save_file(data_file, outfile, m):
-     with open(file=outfile, mode=m) as output_file:
-         json.dump(data_file, output_file, indent=5)
- 
 JSON_DIR = ".\jsonfiles\\"
 # Try load config from a file (if there is a config file)
 config = try_load_from_file(config)
 oneview_client = OneViewClient(config)
 
-# print("Get all the Server Profile Templates and Server Profiles from appliance: ")
-# spts = oneview_client.server_profile_templates.get_all()
-# for spt in spts:
-#    spt_name = (spt['name'])+"_spt_"+".json"
-#    outfile=JSON_DIR+spt_name
-#    save_file(spt, outfile,"w")
-    
-#print("Get list of all server profiles")
-#server_profiles = oneview_client.server_profiles
-#all_profiles = server_profiles.get_all()
-#for profile in all_profiles:
-#    profile_name = (profile['name'])+".json"
-#    outfile=JSON_DIR+profile_name
-#    save_file(profile, outfile,"w")
-NoProfileServers = []
-ProfileServers = []
-count = 0
-
 print("Get list of all the servers from appliance: ")
 servers = oneview_client.server_hardware.get_all()
-# outfile=JSON_DIR+"servers.json"
-# open(outfile,"w")
-#with open(file=outfile, mode="w") as output_file:
-
 server_profiles = oneview_client.server_profiles
-Test = server_profiles.get_all()
+profile_templates = oneview_client.server_profile_templates
+enclosure_resource = oneview_client.enclosures
+enclosure_groups = oneview_client.enclosure_groups
+server_hardware_types = oneview_client.server_hardware_types
+server_hardwares = oneview_client.server_hardware
 
-print("Frame            bay")
+all_templates = profile_templates.get_all()
+for template in all_templates:
+    if template['name'] == config["profile_template"]:
+        print("Template check good")
+server_hardware_type_name = config["server_hardware_type"]
+enclosure_group_name = config["enclosure_group"]
+profile_template_name = config["spt"]
+
+hardware_type = server_hardware_types.get_by_name(server_hardware_type_name)
+enclosure_group = enclosure_groups.get_by_name(enclosure_group_name)
+
 for svr in servers:
     frame = svr['locationUri'].split("/")
-    location = frame[len(frame)-1]
-    bay = str(svr['position'])
-    profile = location+'_'+str(bay)+'_'+config['profile_name']
+    enclosure = enclosure_resource.get_by_uri(svr['locationUri'])
+    bay = svr['position']
     if svr['state'] == "NoProfileApplied":
-        NoProfileServers.append(svr)
-    else:
-        ProfileServers.append(svr)
+        bay = str(svr['position'])
+        if len(bay)==1:
+            i = '00'+bay
+        elif len(bay)==2:
+            i = '0'+bay
+        elif len(bay)==3:
+            i = bay
+        profile_name = config['profile_name'] + i
+        server_template = profile_templates.get_by_name(profile_template_name)
 
-print("These servers do not have profiles")
-for s in NoProfileServers:
-    print ("{0:s} {1:s}\t{2:s}".format(location,bay,profile))
+        print ("Enclusore: {0:s} Bay: {1:3s} Profile: {2:s} Type: {3:s} EG: {4:s}".format(enclosure.data['name'],\
+            (str(bay)),profile_name,\
+            server_hardware_type_name,\
+            config['enclosure_group']))
 
-print("Servers that do have profiles")
-for s in ProfileServers:
-    #pprint(s['name'], location, bay)
-    pprint(s['name'])
+        server_template = profile_templates.get_by_name(profile_template_name)
+        basic_profile_options = dict(name=profile_name, serverProfileTemplateUri=server_template.data["uri"],
+            serverHardwareTypeUri = hardware_type.data["uri"],
+            enclosureGroupUri = enclosure_group.data["uri"]
+        )
+        profile = server_profiles.get_by_name(profile_name)
+        if profile:
+            print("Found profile with name '{}' and uri '{}'".format(profile.data['name'], profile.data['uri']))
+        else:
+            profile = server_profiles.create(basic_profile_options)
+            print("Created profile with name '{}' and uri '{}'".format(profile.data['name'], profile.data['uri']))
