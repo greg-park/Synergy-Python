f5d7d1f8fbbb9a3146ccad6c99f5b7b48189dc2f
greg-park <gregory.park@hpe.com>
2022-07-26 12:16:48 -0500

    Updates to GetServersNoProfile.py

--- GetEnclosures.py
+++ GetEnclosures.py
@@ -2,24 +2,11 @@
 # GetServersNoProfile.py
 # - Script for listing theservers with no profiles.
 #
-#   VERSION 1.1
+#   VERSION 1.0
 #   Date:
 #   Input: config.json
 #   Output: Creates a file in a local subdirectory. jsonfiles, with
 #           list of servers and suggested Profile Name
-#
-#   Changes:
-#       7/25/2022 - major rewrite.  
-#                   - Changed to walking through enclosures
-#                   - Changed naming to read data from config.json
-#                       looks for profile_number and increments from there
-#                   - changed some output formating
-#
-#   Notes:
-#       Program does VERY LITTLE testing to ensure profiles are applied properly
-#       Program does not verify that HW type matches the server found.
-#       Program does not limit the number of profiles to apply.  Its all or nothing
-#
 ############################################################################## 
 
 from fileinput import filename
@@ -55,6 +42,7 @@ enclosure_resource = oneview_client.enclosures
 enclosure_groups = oneview_client.enclosure_groups
 server_hardware_types = oneview_client.server_hardware_types
 server_hardwares = oneview_client.server_hardware
+enclosures = oneview_client.enclosures.get_all()
 
 all_templates = profile_templates.get_all()
 for template in all_templates:
@@ -63,40 +51,15 @@ for template in all_templates:
 server_hardware_type_name = config["server_hardware_type"]
 enclosure_group_name = config["enclosure_group"]
 profile_template_name = config["spt"]
-profile_number = int(config["profile_number"])
 
 hardware_type = server_hardware_types.get_by_name(server_hardware_type_name)
 enclosure_group = enclosure_groups.get_by_name(enclosure_group_name)
-
-enc_count = 0
-
-for enclosure in enclosure_resource.get_all(sort='name:ascending'):
-    #pprint(enclosure)
-    enc_count = enc_count + 1
-
-    for db in enclosure['deviceBays']:
-        bay = str(profile_number)
-        if str(db['profileUri']) == "None":
-            if len(bay)==1:
-                i = '00'+bay
-            elif len(bay)==2:
-                i = '0'+bay
-            elif len(bay)==3:
-                i = bay
-
-            profile_name = config['profile_name'] + "_"+str(enc_count)+"_"+str(i)
-            profile_number = profile_number+1
-
-            server_template = profile_templates.get_by_name(profile_template_name)
-            
-            basic_profile_options = dict(name=profile_name, serverProfileTemplateUri=server_template.data["uri"],
-                serverHardwareTypeUri = hardware_type.data["uri"],
-                enclosureGroupUri = enclosure_group.data["uri"]
-            )
-            profile = server_profiles.get_by_name(profile_name)
-
-            if profile:
-                print("Found profile with name '{}' and uri '{}'".format(profile.data['name'], profile.data['uri']))
-            else:
-                profile = server_profiles.create(basic_profile_options)
-                print("Create profile with name '{}'".format(profile_name))
+sht = "SY 480 Gen10 1"
+for svr in servers:
+    frame = svr['locationUri'].split("/")
+    enclosure = enclosure_resource.get_by_uri(svr['locationUri'])
+    bay = svr['position']
+    print ("Enclusore: {0:s} Bay: {1:3s} State: {2:20s}".format(enclosure.data['name'],\
+            (str(bay)),svr['state'],))
+    server_hardware_type_by_uri = server_hardware_types.get_by_name(sht)
+    pprint(server_hardware_type_by_uri)
