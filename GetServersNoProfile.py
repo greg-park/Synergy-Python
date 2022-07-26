0fc00939b97362608b01dbbe42d74c3197539c90
greg-park <gregory.park@hpe.com>
2022-07-26 16:21:02 -0500

    Update to server profiles.  Ability to update out of compliance profiles added

--- GetServersNoProfile.py
+++ GetServersNoProfile.py
@@ -48,13 +48,14 @@ config = try_load_from_file(config)
 oneview_client = OneViewClient(config)
 
 print("Get list of all the servers from appliance: ")
-servers = oneview_client.server_hardware.get_all()
+# servers = oneview_client.server_hardware.get_all()
+servers = oneview_client.server_hardware
 server_profiles = oneview_client.server_profiles
 profile_templates = oneview_client.server_profile_templates
 enclosure_resource = oneview_client.enclosures
 enclosure_groups = oneview_client.enclosure_groups
 server_hardware_types = oneview_client.server_hardware_types
-server_hardwares = oneview_client.server_hardware
+# server_hardware = oneview_client.server_hardware
 
 all_templates = profile_templates.get_all()
 for template in all_templates:
@@ -64,39 +65,66 @@ server_hardware_type_name = config["server_hardware_type"]
 enclosure_group_name = config["enclosure_group"]
 profile_template_name = config["spt"]
 profile_number = int(config["profile_number"])
+server_model = config["server_model"]
 
 hardware_type = server_hardware_types.get_by_name(server_hardware_type_name)
 enclosure_group = enclosure_groups.get_by_name(enclosure_group_name)
 
 enc_count = 0
+count = 0
+dbcnt = 0
+profile_list = []
 
 for enclosure in enclosure_resource.get_all(sort='name:ascending'):
-    #pprint(enclosure)
+    # pprint(enclosure)
     enc_count = enc_count + 1
-
+    dbcnt = 0
     for db in enclosure['deviceBays']:
-        bay = str(profile_number)
-        if str(db['profileUri']) == "None":
-            if len(bay)==1:
-                i = '00'+bay
-            elif len(bay)==2:
-                i = '0'+bay
-            elif len(bay)==3:
-                i = bay
+        dbcnt = dbcnt+1
+        if "server-hardware" in (str(db['deviceUri'])):
+            count = count+1
+            svr = servers.get_by_uri(db['deviceUri'])
 
-            profile_name = config['profile_name'] + "_"+str(enc_count)+"_"+str(i)
-            profile_number = profile_number+1
+            if (svr.data['serverHardwareTypeUri']) == (hardware_type.data["uri"]):
+                pprint("Enclosure {} Device Bay {} Device {}".format(enc_count, dbcnt, db['devicePresence']))
+                
+                bay = str(profile_number)
+                if str(db['profileUri']) == "None":
+                    if len(bay)==1:
+                        i = '00'+bay
+                    elif len(bay)==2:
+                        i = '0'+bay
+                    elif len(bay)==3:
+                        i = bay
 
-            server_template = profile_templates.get_by_name(profile_template_name)
-            
-            basic_profile_options = dict(name=profile_name, serverProfileTemplateUri=server_template.data["uri"],
-                serverHardwareTypeUri = hardware_type.data["uri"],
-                enclosureGroupUri = enclosure_group.data["uri"]
-            )
-            profile = server_profiles.get_by_name(profile_name)
+                    profile_name = config['profile_name'] + "_"+str(enc_count)+"_"+str(i)
+                    profile_number = profile_number+1
+                    server_template = profile_templates.get_by_name(profile_template_name)
+                    uri = str(svr.data['uri'])
 
-            if profile:
-                print("Found profile with name '{}' and uri '{}'".format(profile.data['name'], profile.data['uri']))
-            else:
-                profile = server_profiles.create(basic_profile_options)
-                print("Create profile with name '{}'".format(profile_name))
+                    basic_profile_options = dict(
+                        name=profile_name,
+                        serverHardwareUri=uri,
+                        affinity="Bay",
+                        serverProfileTemplateUri=server_template.data["uri"],
+                        serverHardwareTypeUri = hardware_type.data["uri"],
+                        enclosureGroupUri = enclosure_group.data["uri"]
+                    )
+
+                    profile = server_profiles.get_by_name(profile_name)
+                    if profile:
+                        print("Found profile with name '{}' and uri '{}'".format(profile.data['name'], profile.data['uri']))
+                    else:
+                        profile = server_profiles.create(basic_profile_options)
+                        print("Create profile with name '{}'".format(profile_name))
+                        # server_profiles.create()
+                        profile_list.append(profile)
+
+print("\nUpdate the profile configurations from server profile template, if needed")
+for s in profile_list:
+    pprint(s.data['name'])
+    profile = server_profiles.get_by_name(s.data['name'])
+    if profile:
+        profile.patch(operation="replace",
+            path="/templateCompliance", value="Compliant")
+       # pprint(profile.data)
